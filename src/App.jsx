565981b09ce34116a7a42eb1fc3a1cb65e4bbe96
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-05-11 20:48:31 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Troque 'Switch' por 'Routes'
 import Login from './pages/login/index.jsx';
 import Register from './pages/register/index.jsx';
-import Home from './pages/home/index.jsx';
+import Homer from './pages/homer/index.jsx';
 
 
 function App() {
     return (
         <Router>
-            <Routes> 
+            <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/home" element={<Home />} />
+                <Route path="/homer" element={<Homer />} />
 
             </Routes>
         </Router>
