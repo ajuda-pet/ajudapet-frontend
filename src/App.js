09917d88a7fdef653625531c9c1f627c1a8e54dd
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-05-14 18:41:18 -0300

    criação dos componentes sidebar e navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/login/index.jsx';
 import Register from './pages/register/index.jsx';
 import Map from './pages/map/index.jsx';
 import Home from './pages/home/index.jsx';
+import NotFoundPage from './pages/notFoundPage/index.jsx';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/map" element={<Map />} />
                 <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFoundPage />} />
 
             </Routes>
         </Router>
