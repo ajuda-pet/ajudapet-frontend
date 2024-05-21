eb0f69f4a2e3a74e477ebf44907ac2ab2e78ab6d
Lebackrobot <lebackman18@gmail.com>
2024-05-21 18:00:26 -0300

    chore: update navbar

--- src/components/molecules/header/index.jsx
+++ src/components/molecules/header/index.jsx
@@ -2,14 +2,18 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import { Button,Form } from 'react-bootstrap'
 import './index.css'
+
+const logout = () => {
+    window.localStorage.removeItem('token')
+    window.location = '/login'
+}
 
 function Header() {
     return (
         <Navbar style={{ borderRadius: '2px', backgroundColor: ' #212f3f'}} expand="lg" data-bs-theme='dark'>
             <Container fluid>
-                <Navbar.Brand href="/"> <img src='./images/logo-sidebar.png' width='100'/></Navbar.Brand>
+                <Navbar.Brand href="/"> <img src='./images/header-logo.png' width='210'/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -33,26 +37,21 @@ function Header() {
                         </Nav.Link> */}
                     </Nav>
 
-                    <Nav className="d-flex" style={{ maxHeight: '100px', fontSize:'25px'}} navbarScroll>
-                        <Nav.Link style={{fontSize: '25px'}} className='circle' href='/'>
+                    <Nav className="d-flex" style={{ maxHeight: '100px', fontSize:'25px',  marginLeft:'40px'}} navbarScroll>
+                        <Nav.Link style={{fontSize: '25px', marginTop: '5px'}} className='circle' href='/about'>
                             <span class="material-symbols-outlined">
                                 help
                             </span>
                         </Nav.Link>
-                        <NavDropdown title="⚙️" id="navbarScrollingDropdown">
-                            {/* <NavDropdown.Item className='circle' href="/">
-                                Tema
+                        <NavDropdown className='' title={<span><img src='./images/group-icon.svg' alt="Ícone de Grupo" style={{ width: '35px' }} /></span>} id="navbarScrollingDropdown">
+                            <NavDropdown.Item href='/login'>
+                                Logar
+                            </NavDropdown.Item>
+                            <NavDropdown.Item className='circle' onClick={logout}>
+                                Sair
                             </NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/">
-                                Action 2
-                            </NavDropdown.Item> */}
                         </NavDropdown>
-                        <Nav.Link className='circle' href="/">
-                            <span style={{fontSize: '40px'}} className="material-symbols-outlined">
-                                groups
-                            </span>
-                        </Nav.Link>
                     </Nav>
 
                 </Navbar.Collapse>
