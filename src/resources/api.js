72c583ad4b579a27849d311e1372d90656d26744
Lebackrobot <lebackman18@gmail.com>
2024-06-13 00:15:14 -0300

    env: config backend env

--- src/resources/api.js
+++ src/resources/api.js
@@ -1,1 +1,7 @@
-export const baseApi = 'https://staging-ajudapet-backend.onrender.com';
+let baseApi = 'https://ajudapet-backend.onrender.com'
+
+if (window.location.href.includes('staging') || window.location.href.includes('localhost')) {
+    baseApi = 'https://staging-ajudapet-backend.onrender.com'
+}
+
+export { baseApi }
